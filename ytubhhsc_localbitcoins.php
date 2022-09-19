502de86bc0e2f3e727977ad15d8999e7410769c1
Your Name <you@example.com>
2022-09-19 14:12:32 +0000

    test

--- ytubhhsc_localbitcoins.php
+++ ytubhhsc_localbitcoins.php
@@ -10,10 +10,10 @@ require_once "vendor/autoload.php";
 $datestamp = date("Y-m-d-g-i-a");      // Current date to append to filename of backup file in format of YYYY-MM-DD
  
 /* CONFIGURE THE FOLLOWING SEVEN VARIABLES TO MATCH YOUR SETUP */
-$dbuser = "vikkin_wallet_vikkin";            // Datvikkin__3107  abase username
-$dbpwd = "wallet_vikkin";            // Database password
+$dbuser = "root";            // Datvikkin__3107  abase username
+$dbpwd = "mWPJ7+Qb-PLP";            // Database password
 $dbname = "vikkin__3107";            // Database name. Use --all-databases if you have more than one
-$filename= "backup-$datestamp.sql.gz";   // The name (and optionally path) of the dump file
+$filename= "backup/backup-$datestamp.sql.gz";   // The name (and optionally path) of the dump file
 $host="localhost";
 
 
