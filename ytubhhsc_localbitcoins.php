1ad8231aa957caeae4716d796d241b7433167b0c
Your Name <you@example.com>
2022-09-19 13:49:01 +0000

    test

--- ytubhhsc_localbitcoins.php
+++ ytubhhsc_localbitcoins.php
@@ -13,7 +13,7 @@ $datestamp = date("Y-m-d-g-i-a");      // Current date to append to filename of 
 $dbuser = "vikkin_wallet_vikkin";            // Datvikkin__3107  abase username
 $dbpwd = "wallet_vikkin";            // Database password
 $dbname = "vikkin__3107";            // Database name. Use --all-databases if you have more than one
-$filename= "backup-$datestamp.sql.gz";   // The name (and optionally path) of the dump file
+$filename= "backup\backup-$datestamp.sql.gz";   // The name (and optionally path) of the dump file
 $host="localhost";
 
 
@@ -28,44 +28,3 @@ $result = passthru($command);
  //////////////////////
 
 
-//PHPMailer Object
-$mail = new PHPMailer(true); //Argument true in constructor enables exceptions
-
-//From email address and name
-$mail->From = "ytubhhsc@alphakoin.com";
-$mail->FromName = "Backup Email";
-
-//To address and name
-$mail->addAddress("globocoins.trade@gmail.com", "backup email ".$filename);
-// $mail->addAddress("ytubhhsc@alphakoin.com"); //Recipient name is optional
-
-//Address to which recipient will reply
-$mail->addReplyTo("susheel3010@gmail.com", "Reply");
-
-//CC and BCC
-$mail->addCC("susheel3010@gmail.com");
-$mail->addBCC("sakshk2019@gmail.com");
-
-
-//Provide file path and name of the attachments
-$mail->addAttachment($filename);        
-
-//$mail->addAttachment( "walletapiv3.tar.gz");    
-
-
-
-// $mail->addAttachment("images/profile.png"); //Filename is optional
-
-//Send HTML or Plain Text email
-$mail->isHTML(true);
-
-$mail->Subject = "Backup Email";
-$mail->Body = "<i>Backup Email</i>";
-$mail->AltBody = "Backup Email";
-
-try {
-    $mail->send();
-    echo "Message has been sent successfully";
-} catch (Exception $e) {
-    echo "Mailer Error: " . $mail->ErrorInfo;
-}
